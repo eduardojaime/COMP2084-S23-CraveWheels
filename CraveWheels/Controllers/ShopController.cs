14a720c3c201d8d6419c909b1c0c40cbf1903bb5
ifotn <rich@infrontofthenet.com>
2023-05-17 20:16:54 -0400

    Add project files.

---
+++ CraveWheels/Controllers/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CraveWheels.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Shop/Restaurant
+        public IActionResult Restaurant()
+        {
+            return View();
+        }
+    }
+}
---
+++ CraveWheels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CraveWheels.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
