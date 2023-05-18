a19cba60c1541aa58086a7632dc461fef7c591eb
ifotn <rich@infrontofthenet.com>
2023-05-17 21:26:35 -0400

    l2/3 - display mock product data

--- CraveWheels/Controllers/ShopController.cs
+++ CraveWheels/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CraveWheels.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CraveWheels.Controllers
 {
@@ -17,8 +18,17 @@ namespace CraveWheels.Controllers
                 return RedirectToAction("Index");
             }
 
+            // use the Product model to make a mock list of products to display
+            var products = new List<Product>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                products.Add(new Product {  ProductId = i, Name = "Product " + i.ToString(), Price = 10 + i });
+            }
+
             ViewData["Name"] = Name;
-            return View();
+            // load the view and pass it the list of products we just made
+            return View(products);
         }
     }
 }
