2eb8a8ac0c4b17faf9efa0cd11a601172c861aae
ifotn <rich@infrontofthenet.com>
2023-06-07 20:54:44 -0400

    l5/3 - start dynamic shopping pages

--- CraveWheels/Controllers/ShopController.cs
+++ CraveWheels/Controllers/ShopController.cs
@@ -1,34 +1,62 @@
-﻿using CraveWheels.Models;
+﻿using CraveWheels.Data;
+using CraveWheels.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CraveWheels.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        // db connection
+        private readonly ApplicationDbContext _context;
+
+        // constructor that requires an instance of the db connection
+        public ShopController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        // GET: /Shop/Restaurant
-        public IActionResult Restaurant(string Name)
+        public IActionResult Index()
         {
-            if (Name == null)
+            // fetch restaurants from db using query
+            var restaurants = _context.Restaurants.OrderBy(r => r.Name).ToList();
+
+            // pass query result to the view
+            return View(restaurants);
+        }
+
+        // GET: /Shop/Restaurant/3
+        public IActionResult Restaurant(int id)
+        {
+            if (id == null)
             {
                 return RedirectToAction("Index");
             }
 
-            // use the Product model to make a mock list of products to display
-            var products = new List<Product>();
+            // fetch products for selected Restaurant
+            var products = _context.Products.Where(p => p.RestaurantId == id).OrderBy(p => p.Name).ToList();
 
-            for (var i = 1; i < 11; i++)
-            {
-                products.Add(new Product {  ProductId = i, Name = "Product " + i.ToString(), Price = 10 + i });
-            }
-
-            ViewData["Name"] = Name;
-            // load the view and pass it the list of products we just made
+            ViewData["Name"] = _context.Restaurants.Find(id).Name;
             return View(products);
         }
+
+        //public IActionResult Restaurant(string Name)
+        //{
+        //    if (Name == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    // use the Product model to make a mock list of products to display
+        //    var products = new List<Product>();
+
+        //    for (var i = 1; i < 11; i++)
+        //    {
+        //        products.Add(new Product {  ProductId = i, Name = "Product " + i.ToString(), Price = 10 + i });
+        //    }
+
+        //    ViewData["Name"] = Name;
+        //    // load the view and pass it the list of products we just made
+        //    return View(products);
+        //}
     }
 }
