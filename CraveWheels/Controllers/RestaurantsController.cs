8edd81ce300699ab38f0e5e3033fc68c9560cee5
ifotn <rich@infrontofthenet.com>
2023-06-14 20:24:18 -0400

    l6/2 - authorization

--- CraveWheels/Controllers/RestaurantsController.cs
+++ CraveWheels/Controllers/RestaurantsController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CraveWheels.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Administrator")]
     public class RestaurantsController : Controller
     {
         private readonly ApplicationDbContext _context;
