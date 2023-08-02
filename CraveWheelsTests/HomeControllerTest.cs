777bd62921b80bcc6febf5f63e98d3add8348ecd
Eduardo Jaime <eduardojaime86@gmail.com>
2023-08-02 19:57:57 -0400

    l12 part 2 homecontrollertest

---
+++ CraveWheelsTests/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// local import > not recommended, move to Usings.cs to import globally
+// using CraveWheels; 
+// using CraveWheels.Controllers;
+
+namespace CraveWheelsTests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        // what to test? 100%???
+        // what's important?
+        [TestMethod]
+        public void IndexReturnsResult() { 
+            // Arrange
+            var controller = new HomeController(null); // null logger instance
+            // Act
+            var result = controller.Index();
+            // Assert > are we returning something?
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- CraveWheelsTests/Usings.cs
+++ CraveWheelsTests/Usings.cs
@@ -1,1 +1,4 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using CraveWheels.Controllers;
+global using CraveWheels.Models;
+global using CraveWheels.Data;
